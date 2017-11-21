33dd6cf89b00282aa4b6811dee176ac00fe9c58f
Harrys <33422967+HarrysV@users.noreply.github.com>
2017-11-21 15:58:12 +0200

    initializes project

---
+++ StaticUtilitiesExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StaticUtilitiesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c1 = "Kostas";
+            string c2 = "Mitsos";
+
+            Utilities.Swap( ref c1, ref c2);
+
+            Console.WriteLine($"Swapped {c1} to {c2}");
+
+            List<int> list = new List<int>() { 4, 4, 4, 4, 4, 5, 5, 5, 6, 6, 2, 6, 9 };
+            List<int> listA = Utilities.Subsequence(list);
+
+            foreach (var item in listA)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StaticUtilitiesExercise/Utilities.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticUtilitiesExercise
+{
+    public static class Utilities
+    {
+        //swaps two integers
+        public static void Swap (ref int num1,ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        //swaps any type of object
+        public static void Swap<T>(ref T num1, ref T num2)
+        {
+            T temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        public static List<int> Subsequence(List<int> list)
+        {
+            int StartIndex = 0;
+            int count = 1;
+            int maxCount = 0;
+            int maxStartIndex = 0;
+           
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i - 1] == list[i])
+                {
+                    count++;
+                    if (i == list.Count - 1 && count > maxCount)
+                    {
+                        maxStartIndex = StartIndex;
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        maxStartIndex = StartIndex;
+                    }
+                    StartIndex = i;
+                    count = 1;
+                }
+
+            }
+            return list.GetRange(maxStartIndex, maxCount);
+        }
+
+
+    }
+}
